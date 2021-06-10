b9733ecaa0811195650ceb185019894c8a77c1a7
Victor Picharelli <picharellivictor@gmail.com>
2021-06-10 19:11:08 -0300

    Nova Versão

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using GenericHost.Events;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Reflection;
 
-namespace Stock.Host
+namespace GenericHost
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            string environmentName = string.Empty;
+            if (args.Length > 0)
+                environmentName = args[0];
+
+            string path = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().Location).LocalPath);
+
+            IHostBuilder host = Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(config =>
+                {
+                    config.SetBasePath(path);
+                    config.AddCommandLine(args);
+
+                    if (string.IsNullOrEmpty(environmentName))
+                        config.AddJsonFile("hostsettings.json", optional: true);
+                    else
+                        config.AddJsonFile($"hostsettings.{environmentName}.json", optional: true);
+                })
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddGatewayServiceLocator();
+                    services.AddStockServiceLocator();
+                    services.AddHostedService<LifetimeEventsHostedService>();
+                    //services.AddSingleton<IGatewayServiceProvider, GatewayServiceProvider>();
+
+                });
+
+            return host;
         }
     }
 }
---
+++ src/Model/DT/DT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
